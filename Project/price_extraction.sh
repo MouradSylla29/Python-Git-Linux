314c064da6de627e834eddcdf7a5057cea5c29b9
Mourad Sylla <mourad.sylla@edu.devinci.fr>
2023-03-22 22:56:21 +0000

    Python git linux project

---
+++ Project/price_extraction.sh
@@ -0,0 +1,1 @@
+curl -s https://moning.co/stocks/AAPL.US | grep -oE 'canvas id="priceChart"[^>]+>' | sed 's/&quot;//g' | grep -oP 'data-graph="\[\K.*?(?=\]".*data-currency="USD")' | grep -oP '\{x:\K[0-9-]+|y:\K[0-9.]+(?=\})' | paste -d ',' - - > Moning_Prices_AAPL.csv
---
+++ Project/scraper.sh
@@ -0,0 +1,3 @@
+html=$(curl -s https://moning.co/stocks/AAPL.US | grep -oE 'canvas id="priceChart"[^>]+>' | sed 's/&quot;//g' | grep -oP 'data-graph="\[\K.*?(?=\]".*data-currency="USD")' | grep -oP '\{x:\K[0-9-]+|y:\K[0-9.]+(?=\})' | paste -d ',' - -)
+price=$(echo "$html")
+echo "$price"
